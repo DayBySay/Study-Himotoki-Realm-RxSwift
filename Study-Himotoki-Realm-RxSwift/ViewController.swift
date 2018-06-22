96505e04ee92a59b01b14536cb9d0f1d8300d8f5
Takayuki Sei <takayuki_sei@voyagegroup.com>
2018-06-22 20:02:57 +0900

    Initial Commit

---
+++ Study-Himotoki-Realm-RxSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Study-Himotoki-Realm-RxSwift
+//
+//  Created by Takayuki Sei on 2018/06/22.
+//  Copyright © 2018年 Takayuki Sei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
